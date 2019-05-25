b3d91fb4258d09b7fdfe2eec6362eb420ebc997b
ranu96 <50720265+ranu96@users.noreply.github.com>
2019-05-25 11:26:32 +0700

    membuat-json

---
+++ 1.php
@@ -0,0 +1,17 @@
+{
+"nama":"ranu hadi pranata"
+"alamat": "sampit"
+"hobby": [
+    "olahraga"
+    "komputer"
+]
+"status": false
+"sekolah":{
+    "sma": "smk n spp sampit"
+    "universitas": "universitas palangka raya"
+}
+"kemampuan":[
+    "komputer"{"baik"}
+    "bahasa"{"cukup baik"}
+]
+}
